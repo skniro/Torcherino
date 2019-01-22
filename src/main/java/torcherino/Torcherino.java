70ff8eeeb057ead73b6e12bad75ac10d6ff6c09b
NinjaPhenix <NinjaPhenix@hotmail.co.uk>
2019-01-22 22:18:52 +0000

    Fixed crash cause i'm stupid Was running client code on server, never noticed cause integrated server worked fine...

--- src/main/java/torcherino/Torcherino.java
+++ src/main/java/torcherino/Torcherino.java
@@ -1,5 +1,8 @@
 package torcherino;
 
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.keybinding.FabricKeyBinding;
 import net.fabricmc.fabric.api.client.keybinding.KeyBindingRegistry;
@@ -15,19 +18,18 @@ import torcherino.block.entity.TorcherinoBlockEntity;
 import torcherino.networking.ClientTickHandler;
 import torcherino.networking.PacketConsumer;
 
-public class Torcherino implements ModInitializer
+public class Torcherino implements ModInitializer, ClientModInitializer
 {
 	public static final BlockEntityType<TorcherinoBlockEntity> TorcherinoBlockEntity = Registry.register(Registry.BLOCK_ENTITY,
             Utils.getId("torcherino"), BlockEntityType.Builder.create(torcherino.block.entity.TorcherinoBlockEntity::new).build(null));
 
+	@Environment(EnvType.CLIENT)
 	public static final FabricKeyBinding torcherinoKeyBind = FabricKeyBinding.Builder.create(Utils.getId("modifier"),
 			InputUtil.Type.KEY_KEYBOARD, 340, "key.categories.misc").build();
 
 	@Override
 	public void onInitialize()
 	{
-		KeyBindingRegistry.INSTANCE.register(torcherinoKeyBind);
-		ClientTickEvent.CLIENT.register(new ClientTickHandler());
 		CustomPayloadPacketRegistry.SERVER.register(Utils.getId("modifier"), new PacketConsumer());
 
 		Utils.blacklistBlock(Blocks.GRASS_BLOCK);
@@ -36,4 +38,13 @@ public class Torcherino implements ModInitializer
 		LootConditions.register(new PlayerModifierLootCondition.Factory());
 		ModBlocks.onInitialize();
 	}
+
+	@Override
+	public void onInitializeClient()
+	{
+		KeyBindingRegistry.INSTANCE.register(torcherinoKeyBind);
+		ClientTickEvent.CLIENT.register(new ClientTickHandler());
+	}
+
+
 }
