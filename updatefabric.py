a337d499aa329fe3fccf86051a8d8d4ce39b2ded
NinjaPhenix <NinjaPhenix@hotmail.co.uk>
2019-03-13 17:26:55 +0000

    Updated update script to have better outputs, also doesn't write to disk if no updates.

--- updatefabric.py
+++ updatefabric.py
@@ -3,28 +3,37 @@ from xml.etree.ElementTree import fromstring
 from argparse import ArgumentParser
 from os.path import exists
 from time import time
-
 maven_url = "https://maven.fabricmc.net/net/fabricmc/{}/maven-metadata.xml"
 
 
 def main(args: dict) -> str:
 	if not exists(args['file']): return f"Failed to update: The file '{args['file']}' does not exist."
 	output = []
-	format_args = dict(fabric_version=get_release('fabric'), loader_version=get_release('fabric-loader'))
-	format_args['minecraft_version'], format_args['yarn_version'] = get_release('yarn').rsplit('.')
+	not_changed = True
+	try:
+		format_args = dict(fabric_version=get_release('fabric'), loader_version=get_release('fabric-loader'))
+		format_args['minecraft_version'], format_args['yarn_version'] = get_release('yarn').rsplit('.')
+	except Exception as e:
+		return str(e)
 	with open(args['file']) as f:
 		for line in f:
-			first = line.split(" ", 1)[0]
-			if first in format_args: output.append(f"{first} = {format_args[first]}\n")
+			if " = " in line:
+				key, value = line.split(" = ", 1)
+				new_value = format_args[key]
+				output.append(f"{key} = {new_value}\n")
+				if new_value not in value: print(f"Updated {key} from {value[0:-1]} to {new_value}"); not_changed = False
 			else: output.append(line)
+	if not_changed: return "Already up to date."
 	with open(args["file"], "w") as f: f.writelines(output)
 	return "Updated successfully."
 
 
 def get_release(package: str) -> str:
-	version = fromstring(urlopen(maven_url.format(package)).read()).find("versioning/release")
-	if hasattr(version, 'text'): return version.text
-	raise ValueError(f"Maven does not have version data for {package}???")
+	try:
+		version = fromstring(urlopen(maven_url.format(package)).read()).find("versioning/release")
+		if hasattr(version, 'text'): return version.text
+	except Exception: pass
+	raise Exception(f"Failed to retrieve maven data for {package}.")
 
 
 if __name__ == "__main__":
