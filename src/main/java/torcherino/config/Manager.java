cf1ef9d61d1d308449e0386ab63e701827c22a98
NinjaPhenix <NinjaPhenix@hotmail.co.uk>
2019-06-24 11:12:21 +0100

    Swapped to lists, dedicated method for deserialising lists.

--- src/main/java/torcherino/config/Manager.java
+++ src/main/java/torcherino/config/Manager.java
@@ -1,16 +1,22 @@
 package torcherino.config;
 
 import net.minecraft.util.ResourceLocation;
-import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+/**
+ * Philosophy of this config: Anything on the right of the equals sign is up to the serializer.
+ * This means comments inside objects must be handled by the serializer.
+ * To put it simply, this is a really basic config format.
+ */
 public class Manager
 {
 
 	private static Map<Class, TypeSerializer> serializers = new HashMap<>();
 	// Special serializer for arrays
-	private static final TypeSerializer<Object> arraySerializer = new ArraySerializer();
+	private static final ListSerializer listSerializer = new ListSerializer();
 	static
 	{
 		serializers.put(Integer.class, new IntegerSerializer());
@@ -26,23 +32,28 @@ public class Manager
 	public static <T> String serialize(T data)
 	{
 		Class dataClass = data.getClass();
-		if (dataClass.isArray())
+		if (data instanceof List)
 		{
-			return arraySerializer.serialize(data);
+			return listSerializer.serialize((List) data);
 		}
 		TypeSerializer serializer = getSerializer(dataClass);
 		if (serializer != null) return serializer.serialize(data);
 		return null;
 	}
 
-	public static <T> T deserialize(String data, Class type)
+	public static <T> List<T> deserializeList(String data, Class<? extends List> type, Class<T> dataType)
 	{
-		if (type.isArray())
+		if (List.class.isAssignableFrom(type))
 		{
-			return (T) arraySerializer.deserialize(data);
+			return listSerializer.deserialize(data, dataType);
 		}
+		return null;
+	}
+
+	public static Object deserialize(String data, Class type)
+	{
 		TypeSerializer serializer = getSerializer(type);
-		if (serializer != null) return (T) serializer.deserialize(data);
+		if (serializer != null) return serializer.deserialize(data);
 		return null;
 	}
 
@@ -52,7 +63,6 @@ public class Manager
 	{
 
 		@Override public String serialize(Integer data){ return data.toString();}
-
 
 		@Override public Integer deserialize(String data){ return Integer.valueOf(data); }
 	}
@@ -85,25 +95,60 @@ public class Manager
 		}
 	}
 
-	private static class ArraySerializer implements TypeSerializer<Object>
+	private static class ListSerializer
 	{
 
-		@Override public String serialize(Object data)
+		public String serialize(List<Object> data)
 		{
 			StringBuilder builder = new StringBuilder("[");
-			for (int i = 0; i < Array.getLength(data); i++)
+			for (int i = 0; i < data.size(); i++)
 			{
 				if (i != 0) builder.append(", ");
-				builder.append(Manager.serialize(Array.get(data, i)));
+				builder.append(Manager.serialize(data.get(i)));
 			}
 			builder.append("]");
 			return builder.toString();
 		}
 
-		@Override public Object deserialize(String data)
+		public <T> List<T> deserialize(String data, Class<T> type)
 		{
-			// todo deserialize
-			return null;
+			if (data.equals("[]")) return new ArrayList<T>();
+			if (data.length() < 3) return null;
+			ArrayList<T> returnValue = new ArrayList<T>();
+			int startPos = 1;
+			int endPos = 1;
+			int level = 0;
+			StringBuilder d = new StringBuilder();
+			while (endPos < data.length() - 1)
+			{
+				char c = data.charAt(endPos);
+				d.append(c);
+				if (c == '(') level += 1;
+				else if (c == '[') level += 2;
+				else if (c == '{') level += 4;
+				else if (c == ')') level -= 1;
+				else if (c == ']') level -= 2;
+				else if (c == '}') level -= 4;
+				else if (c == ',' && level == 0)
+				{
+					// We probably have a value here
+					returnValue.add((T) Manager.deserialize(data.substring(startPos, endPos), type));
+					endPos = startPos = endPos + 1;
+					if (data.charAt(endPos) == ' ') endPos = startPos++;
+				}
+				endPos++;
+			}
+			// Our last value should be here.
+			if (level == 0)
+			{
+				returnValue.add((T) Manager.deserialize(data.substring(startPos, endPos), type));
+			}
+			else
+			{
+				// data is malformed (braces dont match)
+				return null;
+			}
+			return returnValue;
 		}
 	}
 
