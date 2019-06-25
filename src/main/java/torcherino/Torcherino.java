97f30855febca7ad313a041d1d9fbc853844fabf
NinjaPhenix <NinjaPhenix@hotmail.co.uk>
2019-06-25 16:40:39 +0100

    Config Manager is no longer dependant on FML

--- src/main/java/torcherino/Torcherino.java
+++ src/main/java/torcherino/Torcherino.java
@@ -3,11 +3,13 @@ package torcherino;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLPaths;
 import torcherino.blocks.ModBlocks;
 import torcherino.config.ConfigManager;
 import torcherino.config.TorcherinoConfig;
 import torcherino.items.ModItems;
 import torcherino.network.Networker;
+import java.nio.file.Path;
 
 @Mod(Utilities.MOD_ID)
 public class Torcherino
@@ -16,9 +18,9 @@ public class Torcherino
 	{
 		IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
 		TorcherinoTiers.INSTANCE.initialise();
-
-		TorcherinoConfig.INSTANCE = ConfigManager.loadConfig(TorcherinoConfig.class, "Torcherino.cfg");
-
+		Path configPath = FMLPaths.CONFIGDIR.get().resolve("sci4me/Torcherino.cfg");
+		Utilities.LOGGER.info(configPath.toString());
+		TorcherinoConfig.INSTANCE = ConfigManager.loadConfig(TorcherinoConfig.class, configPath);
 		TorcherinoTiers.INSTANCE.registerTier(Utilities.resloc("normal"), 4, 4, 1);
 		TorcherinoTiers.INSTANCE.registerTier(Utilities.resloc("compressed"), 36, 4, 1);
 		TorcherinoTiers.INSTANCE.registerTier(Utilities.resloc("double_compressed"), 324, 4, 1);
--- src/main/java/torcherino/config/ConfigManager.java
+++ src/main/java/torcherino/config/ConfigManager.java
@@ -4,36 +4,37 @@ import blue.endless.jankson.Jankson;
 import blue.endless.jankson.JsonElement;
 import blue.endless.jankson.JsonObject;
 import blue.endless.jankson.impl.SyntaxError;
-import net.minecraftforge.fml.loading.FMLPaths;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Path;
 
 public class ConfigManager
 {
 
 	private static final Logger LOGGER = LogManager.getLogger();
 	private static final Jankson.Builder builder = new Jankson.Builder();
+
 	/**
 	 * Loads a .config file from the config folder and parses it to a POJO.
 	 *
-	 * @param clazz      The class of the POJO that will store all our properties
-	 * @param configName The name of the config file
+	 * @param clazz          The class of the POJO that will store all our properties
+	 * @param configFilePath The path to the config file
 	 * @return A new config Object containing all our options from the config file
 	 */
-	@SuppressWarnings("ConstantConditions") public static <T> T loadConfig(Class<T> clazz, String configName)
+	@SuppressWarnings("ConstantConditions") public static <T> T loadConfig(Class<T> clazz, Path configFilePath)
 	{
 		try
 		{
-			File file = FMLPaths.CONFIGDIR.get().resolve(configName).toFile();
+			File file = configFilePath.toFile();
 			Jankson jankson = builder.build();
 			//Generate config file if it doesn't exist
 			if (!file.exists())
 			{
 				T instance = clazz.newInstance();
-				saveConfig(instance, configName);
+				saveConfig(instance, configFilePath);
 				return instance;
 			}
 			try
@@ -48,33 +49,33 @@ public class ConfigManager
 					JsonObject jsonNew = (JsonObject) jsonElementNew;
 					if (json.getDelta(jsonNew).size() >= 0)
 					{
-						saveConfig(result, configName);
+						saveConfig(result, configFilePath);
 					}
 				}
 				return result;
 			}
 			catch (IOException e)
 			{
-				LOGGER.warn("Failed to load config File {}: {}", configName, e);
+				LOGGER.warn("Failed to load config File {}: {}", configFilePath.getFileName(), e);
 			}
 		}
 		catch (SyntaxError syntaxError)
 		{
-			LOGGER.warn("Failed to load config File {}: {}", configName, syntaxError);
+			LOGGER.warn("Failed to load config File {}: {}", configFilePath.getFileName(), syntaxError);
 		}
 		catch (IllegalAccessException | InstantiationException e)
 		{
-			LOGGER.warn("Failed to create new config file for {}: {}", configName, e);
+			LOGGER.warn("Failed to create new config file for {}: {}", configFilePath.getFileName(), e);
 		}
 		//Something obviously went wrong, create placeholder config
-		LOGGER.warn("Creating placeholder config for {}...", configName);
+		LOGGER.warn("Creating placeholder config for {}...", configFilePath.getFileName());
 		try
 		{
 			return clazz.newInstance();
 		}
 		catch (InstantiationException | IllegalAccessException e)
 		{
-			LOGGER.warn("Failed to create placeholder config for {}: {}", configName, e);
+			LOGGER.warn("Failed to create placeholder config for {}: {}", configFilePath.getFileName(), e);
 		}
 		//this is ... unfortunate
 		return null;
@@ -83,17 +84,17 @@ public class ConfigManager
 	/**
 	 * Saves a POJO Config object to the disk. This is mostly used to create new configs if they don't already exist
 	 *
-	 * @param object     The Config we want to save
-	 * @param configName The filename of our config.
+	 * @param object         The Config we want to save
+	 * @param configFilePath The path to the config file.
 	 */
-	@SuppressWarnings("ResultOfMethodCallIgnored") public static void saveConfig(Object object, String configName)
+	@SuppressWarnings("ResultOfMethodCallIgnored") public static void saveConfig(Object object, Path configFilePath)
 	{
 		Jankson jankson = builder.build();
 		JsonElement json = jankson.toJson(object);
 		String result = json.toJson(true, true);
 		try
 		{
-			File file = FMLPaths.CONFIGDIR.get().resolve(configName).toFile();
+			File file = configFilePath.toFile();
 			if (!file.exists()) file.createNewFile();
 			FileOutputStream out = new FileOutputStream(file, false);
 			out.write(result.getBytes());
@@ -102,7 +103,7 @@ public class ConfigManager
 		}
 		catch (IOException e)
 		{
-			LOGGER.warn("Failed to write to config file {}: {}", configName, e);
+			LOGGER.warn("Failed to write to config file {}: {}", configFilePath.getFileName(), e);
 		}
 	}
 }
