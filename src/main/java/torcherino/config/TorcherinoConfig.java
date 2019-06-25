17621af7ad632e46f5d71eecd08a2087a9b2bf13
NinjaPhenix <NinjaPhenix@hotmail.co.uk>
2019-06-25 15:41:21 +0100

    Change List<ResourceLocation> to ResourceLocation[] (will only be used for querying)

--- src/main/java/torcherino/config/TorcherinoConfig.java
+++ src/main/java/torcherino/config/TorcherinoConfig.java
@@ -3,22 +3,16 @@ package torcherino.config;
 import blue.endless.jankson.Comment;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.loading.FMLLoader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class TorcherinoConfig
 {
 	public static TorcherinoConfig INSTANCE;
 
-	@Comment("\nDefines how much faster randoms ticks are applied compared to what they should be.\nValid Range: 1 to 4096")
-	public int random_tick_rate = 1;
+	@Comment("\nDefines how much faster randoms ticks are applied compared to what they should be.\nValid Range: 1 to 4096") public int random_tick_rate = 1;
 
-	@Comment("Log torcherino placement (Intended for server use)")
-	public boolean log_placement = FMLLoader.getDist().isDedicatedServer();
+	@Comment("Log torcherino placement (Intended for server use)") public boolean log_placement = FMLLoader.getDist().isDedicatedServer();
 
-	@Comment("\nAdd a block by resource location to the blacklist.\nExamples: minecraft:dirt, minecraft:furnace")
-	public List<ResourceLocation> blacklisted_blocks = new ArrayList<>();
+	@Comment("\nAdd a block by resource location to the blacklist.\nExamples: minecraft:dirt, minecraft:furnace") public ResourceLocation[] blacklisted_blocks = new ResourceLocation[]{};
 
-	@Comment("\nAdd a tile entity by resource location to the blacklist.\nExamples: minecraft:furnace, minecraft:mob_spawner")
-	public List<ResourceLocation> blacklisted_tiles = new ArrayList<>();
+	@Comment("\nAdd a tile entity by resource location to the blacklist.\nExamples: minecraft:furnace, minecraft:mob_spawner") public ResourceLocation[] blacklisted_tiles = new ResourceLocation[]{};
 }
