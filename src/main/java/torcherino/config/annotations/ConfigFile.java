4cd88c10c4add8c907b09a4c32c04bae56e26fa8
NinjaPhenix <NinjaPhenix@hotmail.co.uk>
2019-06-25 14:47:02 +0100

    Removed unneeded files, get files working.

--- src/main/java/torcherino/config/annotations/ConfigFile.java
+++ src/main/java/torcherino/config/annotations/ConfigFile.java
@@ -1,13 +1,14 @@
-package io.github.cottonmc.cotton.config.annotations;
+package torcherino.config.annotations;
 
-import io.github.cottonmc.cotton.config.ConfigManager;
-
+import torcherino.config.ConfigManager;
 import java.lang.annotation.*;
 
 @Target(value = ElementType.TYPE)
 @Retention(value = RetentionPolicy.RUNTIME)
 @Inherited
-public @interface ConfigFile {
-    String name();
-    String extension() default ConfigManager.DEFAULT_EXTENSION;
+public @interface ConfigFile
+{
+	String name();
+
+	String extension() default ConfigManager.DEFAULT_EXTENSION;
 }
