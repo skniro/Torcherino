6ea69f9fe51a9e3055cb42d130749d8b4a34ca6b
NinjaPhenix <ninjaphenix@hotmail.co.uk>
2019-07-10 18:38:54 +0100

    Updated to 1.14.4 (pre4), changed dependencies (now uses specific modules) re-added editor config

--- build.py
+++
@@ -1,6 +0,0 @@
-from os import system
-
-system("rmdir build /s /q")
-system("gradlew --stop")
-system("gradlew build")
-input()
--- clean.py
+++ clean.py
@@ -1,7 +1,7 @@
 from os import system
 
-if __name__ == "__main__":
-    system("gradlew cleanLoomBinaries")
-    system("gradlew cleanLoomMappings")
-    system("gradlew genSources")
-    input("Press any key to exit.")
+system("gradlew --stop")
+system("gradlew cleanLoomBinaries")
+system("gradlew cleanLoomMappings")
+system("gradlew genSources")
+input("Press any key to exit.")
