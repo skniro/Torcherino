0d2616acbdbae0a45a172e91ad4051893af40c6c
NinjaPhenix <ninjaphenix@hotmail.co.uk>
2019-07-10 08:18:26 -0700

    changed updateforce.py to clean.py

--- clean.py
+++ clean.py
@@ -1,9 +1,7 @@
 from os import system
 
 if __name__ == "__main__":
-    system("gradlew --stop")
     system("gradlew cleanLoomBinaries")
     system("gradlew cleanLoomMappings")
     system("gradlew genSources")
-    system("gradlew cleanIdea")
     input("Press any key to exit.")
