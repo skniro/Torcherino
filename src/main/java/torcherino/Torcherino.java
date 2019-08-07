9a52119995068bdb7efc4f08cfd702203214660d
NinjaPhenix <ninjaphenix@hotmail.co.uk>
2019-08-07 11:00:12 +0100

    Updated to forge 28.0.45 to fix a crash due to forge changing code location

--- src/main/java/torcherino/Torcherino.java
+++ src/main/java/torcherino/Torcherino.java
@@ -8,7 +8,7 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.gameevent.PlayerEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
