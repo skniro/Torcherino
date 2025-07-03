bca5e0103018d5cd267d4eb1aaf64674e90bb9bd
skniro <46392259+skniro@users.noreply.github.com>
2025-07-03 17:19:10 +0800

    Fix GUI.

--- Fabric/src/main/java/torcherino/client/screen/widgets/StateButtonWidget.java
+++ Fabric/src/main/java/torcherino/client/screen/widgets/StateButtonWidget.java
@@ -1,18 +1,21 @@
 package torcherino.client.screen.widgets;
 
-import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.WidgetSprites;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 
 public abstract class StateButtonWidget extends Button {
     private static final ItemRenderer itemRenderer = Minecraft.getInstance().getItemRenderer();
+    private static final WidgetSprites SPRITES = new WidgetSprites(new ResourceLocation("widget/button"), new ResourceLocation("widget/button_disabled"), new ResourceLocation("widget/button_highlighted"));
     private final Screen screen;
     private Component narrationMessage;
     private Font font;
@@ -37,6 +40,7 @@ public abstract class StateButtonWidget extends Button {
     @Override
     public final void renderWidget(GuiGraphics context, int mouseX, int mouseY, float partialTicks){
         if (visible) {
+            context.blitSprite(SPRITES.get(this.active, this.isHoveredOrFocused()), this.getX(), this.getY(), this.getWidth(), this.getHeight());
             context.renderItem(this.getButtonIcon(),getX() + 2, getY() + 2);
             if (this.isHovered) {
                 context.renderTooltip(this.getFont(), narrationMessage ,getX() + 14, getY() + 18);
